c90a1ce4b5083fcbbad14c5c76cd9800eca4efbe
allait330481 <330481@student.mboutrecht.nl>
2022-05-25 15:08:36 +0200

    .

--- wp-content/themes/wp-dough-main/template-example.php
+++ wp-content/themes/wp-dough-main/template-example.php
@@ -1,10 +1,11 @@
 <?php
 /*
- * Template Name: Custom
+ * Template Name: Allaith
  */
 
 $title = get_the_title();
 $content = get_the_content();
 
 render('views/templates/custom.php', compact('title', 'content'));
+
 ?>
--- wp-content/themes/wp-dough-main/views/templates/home.php
+++ wp-content/themes/wp-dough-main/views/templates/home.php
@@ -13,24 +13,79 @@ if(!$buttonGroup){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Title of the document</title>
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
 
 <header>
-        <a href="#" class="logo">Wonder</a>
-        <nav class="navi">
-            <a href="index.html" class="active">Home</a>
-            <a href="">About Us</a>
-           
-        </nav>
-    </header>
 
-    <section class="main">
+<a href="#" class="logo">Perpark</a>
+
+<nav class="navbar">
+    <a class="active" href="index.html">home</a>
+    <a href="#Categories">Categories</a>
+    <a href="about-us.html">about us</a>
+</nav>
+
+<div class="icons">
+    <i class="fas fa-bars" id="menu-bars"></i>
+    <i class="fas fa-search" id="search-icon"></i>
+</div>
+
+</header>
+
+<section class="main">
         <div>
-            <h3>I build websites and ios-android application</h3>
-            <a href="#projects" class="main-btn">View my work</a>
-            <div class="social-icons">
+            <h2>Het is tijd voor een 
+                sprookjesachtig feestje</h2>
+<button class="button-82-pushable" role="button">
+  <span class="button-82-shadow"></span>
+  <span class="button-82-edge"></span>
+  <span class="button-82-front text">
+    Cheack Beoordeling
+  </span>
+</button>        </div>
+        <img src="img/jeff_koons_theskateroom_post1.jpg" alt="">
+    </section>
+
+    <section class="cards" id="Categories">
+        <h2 class="title">Categories</h2>
+        <div class="content">
+            <div class="card">
+                <div class="img-card">
+                    <img src="img/1.jpg" alt="">
+                </div>
+                <div class="info">
+                    <h3>POPEYE
+                        SKATEBOARD,
+                        JEFF KOONS</h3>
+
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="img-card">
+                    <img src="img/2.jpg" alt="">
+                </div>
+                <div class="info">
+                    <h3>POPEYE
+                        SKATEBOARD,
+                        JEFF KOONS</h3>
+
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="img-card">
+                    <img src="img/3.jpg" alt="">
+                </div>
+                <div class="info">
+                    <h3>POPEYE
+                        SKATEBOARD,
+                        JEFF KOONS</h3>
+
+                </div>
             </div>
         </div>
     </section>
